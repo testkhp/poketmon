4dacee78bfd65109f45f869bc2ffc8945ca7c457
testkhp <khp2337@naver.com>
2024-09-29 16:31:16 +0900

    14일차 과제 제출(상세페이지)

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,17 +16,17 @@ export default function Header({
     const { currentPage, searchWord } = this.state;
 
     let temp = `<div class='header-content' id="title">
-        <img src='/src/img/ball.webp' width=40px height=40px></img>
-        포켓몬 도감</div>`;
+      <img src='/src/img/ball.webp' width=40px height=40px></img>
+      포켓몬 도감</div>`;
 
     //main
     if (!currentPage.includes("/detail")) {
       temp += `<div class="search">
-            <input type="text" placeholder="포켓몬을 검색하세요!" id="search" autocomplete="off" value=${decodeURIComponent(
-              searchWord
-            )}></input>
-            <button id="search-button"><img src="src/img/search.png"></img></button>
-        </div>`;
+          <input type="text" placeholder="포켓몬을 검색하세요!" id="search" autocomplete="off" value=${decodeURIComponent(
+            searchWord
+          )}></input>
+          <button id="search-button"><img src="src/img/search.png"></img></button>
+      </div>`;
     }
 
     return temp;
